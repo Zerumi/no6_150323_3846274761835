ec80de9abf3eb823e53a4a5ac4c9facffec68bad
zerumi <afanasyev09032005@gmail.com>
2023-03-27 17:36:03 +0300

    logger fix
    Signed-off-by: zerumi <afanasyev09032005@gmail.com>

--- client/src/responseLogic/responseWorkers/BaseResponseWorker.java
+++ client/src/responseLogic/responseWorkers/BaseResponseWorker.java
@@ -1,12 +1,15 @@
 package responseLogic.responseWorkers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import responseLogic.dataTransferObjects.BaseResponseDTO;
 import responseLogic.responses.BaseResponse;
 
 public class BaseResponseWorker implements ResponseWorker {
 
+    private static final Logger logger = LogManager.getLogger("io.github.zerumi.lab6");
     @Override
     public void workWithResponse(BaseResponse response, BaseResponseDTO dto) {
-
+        logger.info("got a base response yeah");
     }
 }
